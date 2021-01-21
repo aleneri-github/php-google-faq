8345cdf2831cf5494537001499e5567adeb4e4ed
Alessandro <aleneri242@gmail.com>
2021-01-21 14:41:34 +0100

    Creazione cartella

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Google FAQ</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+
+    <main>
+
+      <div class="container70">
+      
+
+      </div>
+    </main>
+
+  </body>
+</html>
